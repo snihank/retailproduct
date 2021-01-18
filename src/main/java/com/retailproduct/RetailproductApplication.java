5dc780b524984b77e6ddc33d35f6a0573ebe95f3
Shakib Karim <shakib.karim@cognizant.com>
2021-01-18 16:08:54 -0500

    initial commit

---
+++ src/main/java/com/retailproduct/RetailproductApplication.java
@@ -0,0 +1,13 @@
+package com.retailproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailproductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailproductApplication.class, args);
+	}
+
+}
